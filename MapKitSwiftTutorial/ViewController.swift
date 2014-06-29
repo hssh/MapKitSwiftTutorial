d6a7f730f318dc2d82ed1eae789510a0e028d92b
hssh <hssh@hssh.jp>
2014-06-30 01:26:59 +0900

    Implement MapKitTutorial by Swift

--- MapKitSwiftTutorial/ViewController.swift
+++ MapKitSwiftTutorial/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
                             
+    @IBOutlet var mapSearchBar: UISearchBar
+    @IBOutlet var mapView: MKMapView
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        mapSearchBar.delegate = self
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(
+            mapSearchBar.text,
+            completionHandler: {placemarks, error in
+                let placemark: CLPlacemark = placemarks[0] as CLPlacemark
+                let newLocation = placemark.location.coordinate
+        
+                let annotation = MKPointAnnotation()
+                annotation.setCoordinate(newLocation)
+                self.mapView.addAnnotation(annotation)
+        
+                var mr = self.mapView.visibleMapRect
+                let pt = MKMapPointForCoordinate(annotation.coordinate)
+                mr.origin.x = pt.x - mr.size.width * 0.5;
+                mr.origin.y = pt.y - mr.size.height * 0.25;
+                self.mapView.setVisibleMapRect(mr, animated: true)
+            }
+        )
+    }
 }
 
